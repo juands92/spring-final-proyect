df6183c651280db52dc9d68b419a441863f9c028
juands92 <juandguezsuarez@gmail.com>
2024-03-07 00:10:09 +0100

    Initial service configuration for final proyect

---
+++ src/main/java/com/cev/finalproyect/proyectservices/ProyectservicesApplication.java
@@ -0,0 +1,13 @@
+package com.cev.finalproyect.proyectservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectservicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cev/finalproyect/proyectservices/ProyectservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cev.finalproyect.proyectservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
