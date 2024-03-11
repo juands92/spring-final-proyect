7b48598e6dab6c578e6fdac8d2395187b60f8f11
juands92 <juandguezsuarez@gmail.com>
2024-03-11 20:43:08 +0100

    Added authentication for swagger editor

---
+++ src/main/java/com/cev/finalproyect/proyectservices/config/OpenApi3Config.java
@@ -0,0 +1,29 @@
+package com.cev.finalproyect.proyectservices.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+
+@Configuration
+public class OpenApi3Config {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        final String securitySchemeName = "bearerAuth";
+        return new OpenAPI()
+            .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+            .components(new Components().addSecuritySchemes(securitySchemeName,
+                new SecurityScheme()
+                    .name(securitySchemeName)
+                    .type(SecurityScheme.Type.HTTP)
+                    .scheme("bearer")
+                    .bearerFormat("JWT")))
+            .info(new Info().title("CEV Final proyect services"));
+    }
+}
