08e8fa9b5679a11308548bf2cb728293151aecb6
Juan Domínguez <45494442+juands92@users.noreply.github.com>
2024-04-15 14:46:31 +0200

    Update HomeRepository.java

--- src/main/java/com/cev/finalproyect/proyectservices/repository/HomeRepository.java
+++ src/main/java/com/cev/finalproyect/proyectservices/repository/HomeRepository.java
@@ -1,11 +1,9 @@
 package com.cev.finalproyect.proyectservices.repository;
 
-import java.util.UUID;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.cev.finalproyect.proyectservices.domain.Home;
 
-public interface HomeRepository  extends JpaRepository<Home, UUID>{
+public interface HomeRepository  extends JpaRepository<Home, Long>{
 
 }
