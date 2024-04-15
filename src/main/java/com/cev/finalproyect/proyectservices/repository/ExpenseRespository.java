2aafd0de6afc44763f70e78d9c682ac33d9e2cfe
Juan Domínguez <45494442+juands92@users.noreply.github.com>
2024-04-15 14:46:09 +0200

    Update ExpenseRespository.java

--- src/main/java/com/cev/finalproyect/proyectservices/repository/ExpenseRespository.java
+++ src/main/java/com/cev/finalproyect/proyectservices/repository/ExpenseRespository.java
@@ -1,11 +1,9 @@
 package com.cev.finalproyect.proyectservices.repository;
 
-import java.util.UUID;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.cev.finalproyect.proyectservices.domain.Expense;
 
-public interface ExpenseRespository extends JpaRepository<Expense, UUID>{
+public interface ExpenseRespository extends JpaRepository<Expense, Long>{
 
 }
