8af9c71b33d2df751d71b3e940abb5c2836882d1
Juan Domínguez <45494442+juands92@users.noreply.github.com>
2024-05-09 19:34:09 +0200

    Update SecurityConfig.java

--- src/main/java/com/cev/finalproyect/proyectservices/config/SecurityConfig.java
+++ src/main/java/com/cev/finalproyect/proyectservices/config/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200", "http://13.60.94.11"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
         configuration.setAllowCredentials(true);
